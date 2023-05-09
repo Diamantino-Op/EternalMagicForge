2a78b3a55a101f49e6a4838d0749407e13eb8b75
Diamantino Op <Diamantino.op@gmail.com>
2023-05-09 22:51:19 +0200

    s

--- src/main/java/com/diamantino/eternalmagic/client/menu/WandBenchMenu.java
+++ src/main/java/com/diamantino/eternalmagic/client/menu/WandBenchMenu.java
@@ -1,4 +1,63 @@
 package com.diamantino.eternalmagic.client.menu;
 
-public class WandBenchMenu {
+import com.diamantino.eternalmagic.blockentities.WandBenchBlockEntity;
+import com.diamantino.eternalmagic.registration.ModBlocks;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.*;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.items.SlotItemHandler;
+import org.jetbrains.annotations.NotNull;
+
+public class WandBenchMenu extends AbstractContainerMenu {
+    public final WandBenchBlockEntity blockEntity;
+    private final Level level;
+
+    public WandBenchMenu(int id, Inventory inv, FriendlyByteBuf extraData) {
+        this(id, inv, inv.player.level.getBlockEntity(extraData.readBlockPos()));
+    }
+
+    public WandBenchMenu(int id, Inventory inv, BlockEntity entity) {
+        super(ModMenuTypes.GEM_INFUSING_STATION_MENU.get(), id);
+        checkContainerSize(inv, 3);
+        blockEntity = (WandBenchBlockEntity) entity;
+        this.level = inv.player.level;
+
+        addPlayerInventory(inv);
+        addPlayerHotbar(inv);
+
+        this.blockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER).ifPresent(handler -> {
+            this.addSlot(new SlotItemHandler(handler, 0, 12, 15));
+            this.addSlot(new SlotItemHandler(handler, 1, 86, 15));
+            this.addSlot(new SlotItemHandler(handler, 2, 86, 60));
+        });
+    }
+
+    @Override
+    public @NotNull ItemStack quickMoveStack(@NotNull Player player, int index) {
+        return null;
+    }
+
+    @Override
+    public boolean stillValid(Player player) {
+        return stillValid(ContainerLevelAccess.create(level, blockEntity.getBlockPos()), player, ModBlocks.wandBenchBlock.get());
+    }
+
+    private void addPlayerInventory(Inventory playerInventory) {
+        for (int i = 0; i < 3; ++i) {
+            for (int l = 0; l < 9; ++l) {
+                this.addSlot(new Slot(playerInventory, l + i * 9 + 9, 8 + l * 18, 86 + i * 18));
+            }
+        }
+    }
+
+    private void addPlayerHotbar(Inventory playerInventory) {
+        for (int i = 0; i < 9; ++i) {
+            this.addSlot(new Slot(playerInventory, i, 8 + i * 18, 144));
+        }
+    }
 }
